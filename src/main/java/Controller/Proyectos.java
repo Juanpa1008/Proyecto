e71f9060785bcd8a5b9a1dc071a8a8d1ae44eb50
JuanpaCR <108198699+Juanpa1008@users.noreply.github.com>
2022-10-26 23:52:23 -0600

    Actualizacion de controladores, modelos y views

---
+++ src/main/java/Controller/Proyectos.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import AccessData.AccessData;
+import view.AgregarProyectoController;
+import view.CreateProjectController;
+
+import java.sql.ResultSet;
+
+public class Proyectos {
+    /**
+     * función para añadir un proyecto en la base de datos
+     * @param direccion Ubicacion del proyecto
+     * @param nombre Titulo del proyecto
+     * @param categoria Categoria del proyecto
+     * @param fechaCreacion Fecha de la creacion del proyecto
+     * @param repositorio Ubicacion del repositorio del proyecto
+     */
+
+    public static void registrarProyecto(String direccion,String nombre, String categoria, String fechaCreacion, String repositorio){
+            proyecto nuevoProyecto = new proyecto(direccion,nombre,categoria,fechaCreacion,repositorio);
+            AccessData.insertarProyecto(nuevoProyecto.getDireccion(),nuevoProyecto.getName(),nuevoProyecto.getCategory(),nuevoProyecto.getDate().toString(),nuevoProyecto.getDescription());
+            CreateProjectController.alertSuccessfullyAddProject();
+    }
+}/
